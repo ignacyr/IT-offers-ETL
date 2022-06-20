ba994ee467eefaf96dcd7c3e202a4a3a270b2b0d
ignacyr <ignacy.rogatty@gmail.com>
2022-06-20 18:44:29 +0200

    finally postgres connection is fully functional

--- ETL/load_pg.py
+++ ETL/load_pg.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime
 import psycopg2
+import sqlalchemy
 
 
 def column_load_log(column):
@@ -22,31 +23,43 @@ def record_load_log(n):
 
 
 def run():
-    conn = psycopg2.connect(
-        dbname="dwh",
-        user="app_user",
-        host="it-offers.c9umk0ew1r8h.us-east-1.rds.amazonaws.com",
-        password="analyze2137"
-    )
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS offers (id serial primary key);")
+    with open("/home/ignacyr/Desktop/password_app_user") as f:
+        password_app_user = f.readline().strip()
+
+    conn = sqlalchemy.create_engine(
+        f'postgresql://app_user:{password_app_user}@it-offers.c9umk0ew1r8h.us-east-1.rds.amazonaws.com:5432/dwh')
+
+    # conn = psycopg2.connect(
+    #     dbname="dwh",
+    #     user="app_user",
+    #     host="it-offers.c9umk0ew1r8h.us-east-1.rds.amazonaws.com",
+    #     password=password_app_user
+    # )
+
+    # cur = conn.cursor()
+    conn.execute("CREATE TABLE IF NOT EXISTS offers (id serial primary key);")
     staging_df = pd.read_csv('staging.csv', index_col=0)
 
     schema_list = list(staging_df.columns)
 
     for column in schema_list:
         try:
-            cur.execute(f"ALTER TABLE offers ADD COLUMN {column} integer;")
+            if column in ("min_salary", "max_salary", "date"):
+                conn.execute(f"ALTER TABLE offers ADD COLUMN {column} integer;")
+            elif column in ("title", "level", "skills", "category", "company", "link",
+                            "trainee", "junior", "mid", "senior", "expert"):
+                conn.execute(f"ALTER TABLE offers ADD COLUMN {column} varchar;")
+            else:
+                conn.execute(f"ALTER TABLE offers ADD COLUMN {column} boolean;")
             column_load_log(column)
         except psycopg2.OperationalError as error:
-            print(f"{error}: {column} is already in table ########################")
+            print(f"{column} is already in table ########################")
+        except sqlalchemy.exc.ProgrammingError as error:
+            print(f"{column} is already in table ########################")
 
     # staging_df.to_sql('offers', con=conn, if_exists='append', index=False)
+    staging_df.to_sql(name='offers', con=conn, if_exists='append', index=False)
     record_load_log(len(staging_df))
-
-    conn.commit()
-    cur.close()
-    conn.close()
 
 
 if __name__ == '__main__':
